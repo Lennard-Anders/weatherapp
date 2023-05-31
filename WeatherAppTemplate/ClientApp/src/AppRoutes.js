24e34972e2fb06107b376a58af828321a0b15f45
ibo ay <ibrahim.halil.aydiner@hs-furtwangen.de>
2023-05-31 17:01:08 +0200

    Titel, Icon, Navbar Änderung

--- WeatherAppTemplate/ClientApp/src/AppRoutes.js
+++ WeatherAppTemplate/ClientApp/src/AppRoutes.js
@@ -1,5 +1,4 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
+import { Dashboard } from "./components/Dashboard";
 import { Home } from "./components/Home";
 
 const AppRoutes = [
@@ -8,13 +7,9 @@ const AppRoutes = [
     element: <Home />
   },
   {
-    path: '/counter',
-    element: <Counter />
+    path: '/dashboard',
+    element: <Dashboard />
   },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
 ];
 
 export default AppRoutes;
--- WeatherAppTemplate/ClientApp/src/components/NavMenu.js
+++ WeatherAppTemplate/ClientApp/src/components/NavMenu.js
@@ -24,19 +24,14 @@ export class NavMenu extends Component {
   render() {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">WeatherAppTemplate</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                <NavbarBrand tag={Link} to="/"><img alt="WeatherApp Logo" src="/Bilder/Weather360logo.png" height="100"></img></NavbarBrand>
+                <NavLink tag={Link} className="text-dark" to="/"><input className="suche" type="search" placeholder="Geben Sie einen Ort ein!"></input></NavLink>
+                <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse className="flex" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
               </NavItem>
             </ul>
           </Collapse>
